4dd8fc396bd3de31a582084891a730aafde83ac2
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-02-28 15:56:13 +0530

    Move common defuns to ./gh-stdlib from integers section

--- chapters/integers/compiler.lisp
+++ chapters/integers/compiler.lisp
@@ -1,21 +1,3 @@
-;; Refer the paper for these values. Take care to keep it in sync with the runtime written in C
-(defconstant fixnum-shift 2)
-
-(defun integer-p (value)
-  "Checks if a value is an integer.
-
-  Args:
-    value: The value to check.
-
-  Returns:
-    T if the value is an integer, nil otherwise."
-  (and (numberp value) (equal (floor value) value)))
-
-(defun immediate-rep (x)
-  "Converts our toy scheme representation of integers to machine representation"
-  (cond
-    ((integer-p x) (ash x fixnum-shift))))
-
 (defun compile-program (x)
   "Currently only support immediate representations of integers"
   (emit "movz x0, #~a" (immediate-rep x))
--- gh-stdlib/defuns.lisp
+++ gh-stdlib/defuns.lisp
@@ -1,3 +1,22 @@
+;; Refer the paper for these values. Take care to keep it in sync with the runtime written in C
+(defconstant fixnum-shift 2)
+
+(defun integer-p (value)
+  "Checks if a value is an integer.
+
+  Args:
+    value: The value to check.
+
+  Returns:
+    T if the value is an integer, nil otherwise."
+  (and (numberp value) (equal (floor value) value)))
+
+(defun immediate-rep (x)
+  "Converts our toy scheme representation of integers to machine representation"
+  (cond
+    ((integer-p x) (ash x fixnum-shift))))
+
 (defun emit (asm &rest args)
-    (apply #'format (cons t (cons asm args)))
-    (format t "~%"))
+  "Assembly printer. Currently only to stdout"
+  (apply #'format (cons t (cons asm args)))
+  (format t "~%"))
